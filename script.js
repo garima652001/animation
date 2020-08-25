9f33b040d205dbc0618ac16e174eda5336210565
garima652001 <garima1913076@akgec.ac.in>
2020-08-26 01:05:47 +0530

    Changed js code for canvas edges

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
 
-	var canvas = document.querySelector("canvas"); 
+	var canvas = document.getElementById("canvas"); 
 
 	canvas.width = window.innerWidth; 
 	canvas.height = window.innerHeight; 
 
 	var ball = canvas.getContext('2d'); 
 
-	// x and y are the coordinates of the circle 
-	// vx and vy are the relative speeds 
+	// x and y are coordinates of the circle 
 	var x = Math.floor(Math.random() * innerWidth); 
-	var y = Math.floor(Math.random() * innerHeight); 
-	var rx = Math.floor(Math.random() * 5); 
-	var ry = Math.floor(Math.random() * 5); 
-	var radius = 50; 
+	var y = Math.floor(Math.random() * innerHeight);   
+
+	// rx and ry are the relative speeds 
+	var rx = Math.floor(Math.random() * 10); 
+	var ry = Math.floor(Math.random() * 10); 
+	var radius = 60; 
 
 	animate(); 
 	
-	// This function will do the animation 
+	//animation fn
 	function animate() { 
 		requestAnimationFrame(animate); 
 
-		// It clears the specified pixels within 
-		// the given rectangle 
+		
 		ball.clearRect(0, 0, innerWidth, innerHeight); 
 
-		// Creating a circle 
+		// Creating circle 
 		ball.beginPath(); 
-		ball.strokeStyle = "black"; 
+		ball.strokeStyle = "grey"; 
 		ball.arc(x, y, radius, 0, Math.PI * 2, false); 
 		ball.stroke(); 
 
-		// Conditions so that the ball bounces 
-		// from the edges 
+	//Condition for edges
 		if (radius + x > innerWidth) 
-			rx = 0 - rx; 
-
+			 rx = 0 - rx;
+		    
 		if (x - radius < 0) 
-			rx = 0 - rx; 
+		 	rx = 0 - rx; 
 
-		if (y + radius > innerHeight) 
+		if (radius + y > innerHeight) 
 			ry = 0 - ry; 
 
 		if (y - radius < 0) 
 			ry = 0 - ry; 
 
 		x = x + rx; 
-		y = y + ry; 
+		y = y + ry;
+
+		//Condition for fillstyle
+		if (radius + x == innerWidth || radius+ x == 60)
+		{
+	       ball.fillStyle = "black"; 
+		   ball.fill(); 
+		}
+		if (radius + y == innerHeight || radius+ y == 60)
+		{
+	       ball.fillStyle = "black"; 
+		   ball.fill(); 
+		}
     }
 
