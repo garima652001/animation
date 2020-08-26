49c24837c86f06479bc72c6397856c1d998dbf90
garima652001 <garima1913076@akgec.ac.in>
2020-08-26 15:43:01 +0530

    Modified canvas size and other elements

--- script.js
+++ script.js
@@ -5,39 +5,41 @@
 	canvas.height = window.innerHeight; 
 
 	var ball = canvas.getContext('2d'); 
-
-	// x and y are coordinates of the circle 
+	
+	var radius = 60; //radius of ball
+	
 	var x = Math.floor(Math.random() * innerWidth); 
-	var y = Math.floor(Math.random() * innerHeight);   
+	var y = Math.floor(Math.random() * innerHeight);
+	// x and y are positions of the circle    
 
+	var rx = Math.floor(Math.random() * 9); 
+	var ry = Math.floor(Math.random() * 9); 
 	// rx and ry are the relative speeds 
-	var rx = Math.floor(Math.random() * 10); 
-	var ry = Math.floor(Math.random() * 10); 
-	var radius = 60; 
 
 	animate(); 
 	
 	//animation fn
 	function animate() { 
-		requestAnimationFrame(animate); 
+		requestAnimationFrame(animate); //animate fn calls itself  thereby creating a request to next frame
 
-		
-		ball.clearRect(0, 0, innerWidth, innerHeight); 
+		ball.clearRect(0, 0, innerWidth, innerHeight); //Clears canvas acc to specified dimensions
 
 		// Creating circle 
 		ball.beginPath(); 
-		ball.strokeStyle = "grey"; 
+		ball.fillStyle = "black"; 
 		ball.arc(x, y, radius, 0, Math.PI * 2, false); 
-		ball.stroke(); 
-
-	//Condition for edges
+		ball.fill(); 
+		ball.strokeStyle="white";
+		ball.stroke();
+	
+	 //Condition for edges
 		if (radius + x > innerWidth) 
 			 rx = 0 - rx;
 		    
 		if (x - radius < 0) 
 		 	rx = 0 - rx; 
 
-		if (radius + y > innerHeight) 
+		if (radius + y >innerHeight ) 
 			ry = 0 - ry; 
 
 		if (y - radius < 0) 
@@ -46,15 +48,15 @@
 		x = x + rx; 
 		y = y + ry;
 
-		//Condition for fillstyle
-		if (radius + x == innerWidth || radius+ x == 60)
+		//Condition for collision with edges
+		if (radius + x == innerWidth || x - radius == 0)
 		{
-	       ball.fillStyle = "black"; 
+	       ball.fillStyle = "white"; 
 		   ball.fill(); 
 		}
-		if (radius + y == innerHeight || radius+ y == 60)
+		if (radius + y == innerHeight || y - radius == 0)
 		{
-	       ball.fillStyle = "black"; 
+	       ball.fillStyle = "white"; 
 		   ball.fill(); 
 		}
     }
